28880b3ad8dbce88ac28049e7af3482a10ff0147
webberr <webberr@gmail.com>
2018-12-11 08:24:57 -0800

    fixing build

---
+++ build/precache-manifest.762bfd14907c42bd9cef62bf8e2d8301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "92b9560c076c67d57887",
+    "url": "/static/js/main.92b9560c.chunk.js"
+  },
+  {
+    "revision": "a7800656b10bb81439c1",
+    "url": "/static/js/1.a7800656.chunk.js"
+  },
+  {
+    "revision": "92b9560c076c67d57887",
+    "url": "/static/css/main.b84eeaf2.chunk.css"
+  },
+  {
+    "revision": "a7800656b10bb81439c1",
+    "url": "/static/css/1.3a92cb2a.chunk.css"
+  },
+  {
+    "revision": "17f9d98002068b5d2e663636cef61c63",
+    "url": "/index.html"
+  }
+];
